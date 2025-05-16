6e8238897c4a311ce3b0b5eb38beb1c12c1df4ae
gardin <gervaisgardin@gmail.com>
2025-05-16 19:44:46 -0400

    Ajout de la fonction afficherMessage pour le bouton

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function afficherMessage() {
+  alert("Bonjour ! Merci d'avoir cliqué sur le bouton 😊");
+}
